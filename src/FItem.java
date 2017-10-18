e22143043e95c831b5bd8cb08df7e6a7ef815b01
Calibretto <Calibretto@NEOTOKYO.fritz.box>
2017-10-19 01:10:51 +0200

    last comments

--- src/FItem.java
+++ src/FItem.java
@@ -10,6 +10,9 @@ public class FItem {//quantity of a item in a blueprint
     	}
     
     int numberofStacks (String name, int quantity) {
+	//wie groﬂ ist die stacksize des items? 
+	//wie viele stacks davon sind im blueprint? 
+	//die anzahl der stacks wird sp‰ter auf den trainwagon verteilt 
 	int stack = 5;
 	return stack;	
     	}
