dc62be272c2e3207bdfd860dbbffe9ef2bbf14d3
Calibretto <github.com@sentinelx.de>
2017-10-24 18:34:24 +0200

    input = output
    seems to work. 
    converting the baos to string to ByteArray broke the zip. 
    see also: https://repl.it/NBqy/13
    input=output check at the end fails despite same characters

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,12 +3,13 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
-//import java.util.Base64.Encoder;
-//import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+import java.util.Base64.Decoder;
 import java.util.zip.Deflater;
 import java.util.zip.InflaterInputStream;
 import java.util.zip.DeflaterOutputStream;
 import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 
 class Main {
@@ -18,39 +19,43 @@ class Main {
       try{
   System.out.println("microsample-----: " + microsample);
   System.out.println("--------------------------------------");
-        microsample = microsample.substring(1); //remove leading 0, its not part of the zipped String
+
+        microsample = microsample.substring(1); //remove leading 0
+
+        // Base64.Decoder decoderVar = Base64.getDecoder();	
+  		  byte[] bytearray_decoderVar = Base64.getDecoder().decode(microsample.getBytes(StandardCharsets.UTF_8));  
+
+        // ByteArrayInputStream bais = new ByteArrayInputStream(bytearray_decoderVar);
+  	    InflaterInputStream iis = new InflaterInputStream(new ByteArrayInputStream(bytearray_decoderVar));
   
-        Base64.Decoder decoderVar = Base64.getDecoder();	
-  		  byte[] bytearray_decoderVar = decoderVar.decode(microsample.getBytes());//ich verwende eine methode des ausgangsobjektes und schütte den damit erzeugten input input in das objekt und verwende dazu eine methode  
-  
-  	    ByteArrayInputStream bais = new ByteArrayInputStream(bytearray_decoderVar);
-  	    InflaterInputStream iis = new InflaterInputStream(bais);
-  
-  	    String uglyJSONreturn = "";//es muss erstmal ein leerer string erzeugt werden
-  	    byte[] buffer = new byte[8219];//a buffer for i dunno
+  	    String uglyJSONreturn = "";   //es muss erstmal ein leerer string erzeugt werden
+  	    byte[] buffer = new byte[1];   //a buffer for i dunno
   	    int returnStringLength = -1;
   	    while ((returnStringLength = iis.read(buffer)) != -1) {
-  	  	uglyJSONreturn += new String(Arrays.copyOf(buffer, returnStringLength));
-  	     	}
+  	  	  uglyJSONreturn += new String(Arrays.copyOf(buffer, returnStringLength));
+    	     	}
+
   System.out.println("uglyJSONreturn-----: " + uglyJSONreturn);
   System.out.println("--------------------------------------");
-        //--------------------------------------
 
-      String jsonString = uglyJSONreturn;
+        //übergabe von deCode zu enCode --------------------------------------
+        String json2ZIP = uglyJSONreturn; 
+        //übergabe von deCode zu enCode --------------------------------------
       
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DeflaterOutputStream dos = new DeflaterOutputStream(baos, new Deflater(9)); //the Deflater sets compressionlevel
-        dos.write(jsonString.getBytes());//getbytes returns a bytearray
+        DeflaterOutputStream dos = new DeflaterOutputStream(baos, new Deflater(9)); 
+            //(the Deflater sets compressionlevel)
+        dos.write(json2ZIP.getBytes(StandardCharsets.UTF_8));   //getBytes() returns a bytearray
         dos.flush();
         dos.close();
-        String outputToSting = baos.toString();
 
-  System.out.println("outputToSting-----: " + outputToSting);    	
+  System.out.println("baos.toString()-----: " + baos.toString());    	
   System.out.println("--------------------------------------");
 
-      	byte[] b64BA = outputToSting.getBytes();
-        Base64.Encoder enc = Base64.getEncoder();
-        String toBeReturnedString = "0" + enc.encodeToString(b64BA);
+        byte[] charset = baos.toByteArray(); //baos is a outputStream, der zum byteArray werden muss
+        String toBeReturnedString = Base64.getEncoder().encodeToString(charset);
+
+        toBeReturnedString = "0" + toBeReturnedString;//make BluePrint String again
 
   System.out.println("toBeReturnedString-----: "+toBeReturnedString);          
   System.out.println("--------------------------------------");
@@ -58,7 +63,7 @@ class Main {
           microsample = "0" + microsample; //add leading 0 for final comparision
 
   System.out.println("is input = output -----: " + (toBeReturnedString == microsample));          
-  System.out.println("--------------------------------------");
+  System.out.println("--------------------------------------");//this check fails don't know why
 
         }
 	            catch(Exception e){
@@ -66,7 +71,3 @@ class Main {
         }
     }
 }
-
-// wrong output replit: 0eO+/vUXvv73vv70KQiEQBO+/vX/vv73vv70N77+977+9Ru+/vSsR77+977+9D++/ve+/ve+/ve+/ve+/vXLvv73fswh6GRjvv73vv73vv73vv71x77+9Uu+/vXbvv70O77+9Hnrvv70G77+977+977+977+977+9NXnvv73vv70TQV0SMFrvv73vv70e77+9U++/vSDvv73cpu+/ve+/ve+/vS/vv73vv70c77+9Tghd77+977+977+9Lwjvv73vv73vv714LglP77+977+9Cxdr77+9cj1bczFj77+9Ae+/vQEwUQ==
-// wrong output eclpis: 0eJxFzdEKQiEQBNB/mecNyuRG+ysRobAPgq6i3uhy8d+zCHoZGJjD7PBxlVKDdvAO0R56Pwa+/cr20DV5qeATQV0SMFqOrh6KU4kglNymyfrxL/CRsM0c404IXdLc/y8I0fmpeC4JT6ntCxdrzXI9W3MxY7wBqAEwUQ==
-//    String jsonString="{\"blueprint\":{\"entities\":[{\"entity_number\":1,\"name\":\"solar-panel\",\"position\":{\"x\":0,\"y\":0}}],\"item\":\"blueprint\",\"label\":\"x\",\"version\":64426934272}}";
