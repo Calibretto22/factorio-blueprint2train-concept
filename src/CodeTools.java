68768395bd1152943b1f6ce206d9d644c3c89680
Calibretto <Calibretto@NEOTOKYO.fritz.box>
2017-10-29 00:59:25 +0200

    Maven ... whatever this is.

--- src/CodeTools.java
+++ src/CodeTools.java
@@ -3,23 +3,18 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Base64.Encoder;
-import java.util.Base64.Decoder;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
 public class CodeTools {
-    String microsample = "0eNpFzdEKQiEQBNB/mecNyuRG+ysRobAPgq6i3uhy8d+zCHoZGJjD7PBxlVKDdvAO0R56kAa+/cr20DV5qeATQV0SMFqOrh6KU4kglNymyfrxL/CRsM0c404IXdLc/y8I0fmpeC4JT6ntCxdrzXI9W3MxY7wBqAEwUQ==";
 
-    String addFirstZero (String input) { //für später, wenn der BP-String wieder ausgegeben werden soll
-	return "0"+input;
-	}
-    String decodeBPstringtoJSON(String fbpString) throws IOException {	//konvertiert den BPstring zu einem JSON-String
+    static String decodeBPstringtoJSON(String fbpString) throws IOException {	//konvertiert den BPstring zu einem JSON-String
     	fbpString.trim();//entfernt leerzeichen vom string, vorne und hinten 
-    	if (fbpString.subSequence(0, 1) == "0") {//prüft ob erstes Zeichen 0 ist
+//    	if (fbpString.subSequence(0, 1) == "0") {//prüft ob erstes Zeichen 0 ist
     	    fbpString = fbpString.substring(1);
 
     	    Base64.Decoder decoderVar = Base64.getDecoder();	//ich mache ein objekt in der Form des Outputs der getDecoder-Methode.
@@ -35,31 +30,20 @@ public class CodeTools {
 	    	}
 	    //System.out.println(uglyjsonreturn);
 	    return uglyJSONreturn;
-    	}//end leading 0-check
-    	return "Error - no Factorio Bluprint / first Character of String is not zero"; 
+//    	}//end leading 0-check
+//    	return "Error - this is no Factorio Bluprint: " + fbpString; 
     	}
 
-    String encodeJSONtoBlueprintString (String jsonString) throws IOException {//hier fummel ich das JSON-Objekt wieder zu einem FBP zusammen (ZIP, BASE64, +0)
-	//möglicherweise übergebe ich keinen String sondern ein JSONobjekt - das wird sich mit der Zeit zeigen
-	if (jsonString == "") {jsonString="{\"blueprint\":{\"entities\":[{\"entity_number\":1,\"name\":\"solar-panel\",\"position\":{\"x\":0,\"y\":0}}],\"item\":\"blueprint\",\"label\":\"x\",\"version\":64426934272}}";}
-	//ohne escapes: {"blueprint":{"entities":[{"entity_number":1,"name":"solar-panel","position":{"x":0,"y":0}}],"item":"blueprint","label":"x","version":64426934272}}
+    static String encodeJSONtoBlueprintString (String jsonString) throws IOException {//hier fummel ich das JSON-Objekt wieder zu einem FBP zusammen (ZIP, BASE64, +0)
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DeflaterOutputStream dos = new DeflaterOutputStream(baos);
-        dos.write(jsonString.getBytes());//getbytes returned ein bytearray
+        DeflaterOutputStream dos = new DeflaterOutputStream(baos, new Deflater(9)); 
+            //(the Deflater sets compressionlevel)
+        dos.write(jsonString.getBytes(StandardCharsets.UTF_8));   //getBytes() returns a bytearray
         dos.flush();
         dos.close();
-  System.out.println("baos-----: "+baos);
-    System.out.println("--------------------------------------");
-    	String outputToSting = baos.toString();
-  System.out.println("b64BA-----: "+outputToSting);    	
-     System.out.println("--------------------------------------");
-    	byte[] b64BA = outputToSting.getBytes();
-//    	baos.toByteArray(b64BA);
-        Base64.Encoder enc= Base64.getEncoder();
-        String toBeReturnedString = "0" + enc.encodeToString(b64BA);
-  System.out.println("toBeReturnedString-----: "+toBeReturnedString);          
-    System.out.println("--------------------------------------");
-	//System.out.println(toBereturnedString);
+        byte[] charset = baos.toByteArray(); //baos is a outputStream, der zum byteArray werden muss
+        String toBeReturnedString = Base64.getEncoder().encodeToString(charset);
+        toBeReturnedString = "0" + toBeReturnedString;//make BluePrint String again
 	return toBeReturnedString;
 	}
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -2,72 +2,60 @@ package test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Base64;
-import java.util.Base64.Encoder;
-import java.util.Base64.Decoder;
 import java.util.zip.Deflater;
 import java.util.zip.InflaterInputStream;
 import java.util.zip.DeflaterOutputStream;
 import java.util.Arrays;
 import java.nio.charset.StandardCharsets;
 
-
 class Main {
-    public static void main(String args[]){
+    
+public static String decodeBPstringtoJSON(String fbpString) throws IOException {	//konvertiert den BPstring zu einem JSON-String
+	fbpString.trim();//entfernt leerzeichen vom string, vorne und hinten 
+//	if (fbpString.subSequence(0, 1) == "0") {//prüft ob erstes Zeichen 0 ist
+	    fbpString = fbpString.substring(1);
+
+	    Base64.Decoder decoderVar = Base64.getDecoder();	//ich mache ein objekt in der Form des Outputs der getDecoder-Methode.
+	    byte[] bytearray_decoderVar = decoderVar.decode(fbpString.getBytes());//ich verwende eine methode des ausgangsobjektes und schütte den damit erzeugten input input in das objekt und verwende dazu eine methode  
+	    ByteArrayInputStream bais = new ByteArrayInputStream(bytearray_decoderVar);
+	    InflaterInputStream iis = new InflaterInputStream(bais);
+	    
+	    String uglyJSONreturn = "";//es muss erstmal ein leerer string erzeugt werden
+	    byte[] buffer = new byte[5];//ein buffer für i dunno
+	    int returnStringLength = -1;
+	    while ((returnStringLength = iis.read(buffer)) != -1) {
+		uglyJSONreturn += new String(Arrays.copyOf(buffer, returnStringLength));
+	    	}
+	    //System.out.println(uglyjsonreturn);
+	    return uglyJSONreturn;
+//	}//end leading 0-check
+//	return "Error - this is no Factorio Bluprint: " + fbpString; 
+	}
+
+public static String encodeJSONtoBlueprintString (String jsonString) throws IOException {//hier fummel ich das JSON-Objekt wieder zu einem FBP zusammen (ZIP, BASE64, +0)
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DeflaterOutputStream dos = new DeflaterOutputStream(baos, new Deflater(9)); 
+        //(the Deflater sets compressionlevel)
+    dos.write(jsonString.getBytes(StandardCharsets.UTF_8));   //getBytes() returns a bytearray
+    dos.flush();
+    dos.close();
+    byte[] charset = baos.toByteArray(); //baos is a outputStream, der zum byteArray werden muss
+    String toBeReturnedString = Base64.getEncoder().encodeToString(charset);
+    toBeReturnedString = "0" + toBeReturnedString;//make BluePrint String again
+	return toBeReturnedString;
+	}
+    
+public static void main(String args[]) throws IOException{
     String microsample = "0eNpFzdEKQiEQBNB/mecNyuRG+ysRobAPgq6i3uhy8d+zCHoZGJjD7PBxlVKDdvAO0R56kAa+/cr20DV5qeATQV0SMFqOrh6KU4kglNymyfrxL/CRsM0c404IXdLc/y8I0fmpeC4JT6ntCxdrzXI9W3MxY7wBqAEwUQ==";
 
-      try{
-  System.out.println("microsample-----: " + microsample);
-  System.out.println("--------------------------------------");
+	System.out.println("all in one");
+	System.out.println("Microsample inp: " + microsample);
+	String uglyJSON = decodeBPstringtoJSON(microsample);
+	System.out.println("Microsample JSON: " + uglyJSON);
+	String returnBP = encodeJSONtoBlueprintString(uglyJSON);
+	System.out.println("Microsample 2BP: " + returnBP); 
 
-        microsample = microsample.substring(1); //remove leading 0
-
-        // Base64.Decoder decoderVar = Base64.getDecoder();	
-  		  byte[] bytearray_decoderVar = Base64.getDecoder().decode(microsample.getBytes(StandardCharsets.UTF_8));  
-
-        // ByteArrayInputStream bais = new ByteArrayInputStream(bytearray_decoderVar);
-  	    InflaterInputStream iis = new InflaterInputStream(new ByteArrayInputStream(bytearray_decoderVar));
-  
-  	    String uglyJSONreturn = "";   //es muss erstmal ein leerer string erzeugt werden
-  	    byte[] buffer = new byte[1];   //a buffer for i dunno
-  	    int returnStringLength = -1;
-  	    while ((returnStringLength = iis.read(buffer)) != -1) {
-  	  	  uglyJSONreturn += new String(Arrays.copyOf(buffer, returnStringLength));
-    	     	}
-
-  System.out.println("uglyJSONreturn-----: " + uglyJSONreturn);
-  System.out.println("--------------------------------------");
-
-        //Ã¼bergabe von deCode zu enCode --------------------------------------
-        String json2ZIP = uglyJSONreturn; 
-        //Ã¼bergabe von deCode zu enCode --------------------------------------
-      
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DeflaterOutputStream dos = new DeflaterOutputStream(baos, new Deflater(9)); 
-            //(the Deflater sets compressionlevel)
-        dos.write(json2ZIP.getBytes(StandardCharsets.UTF_8));   //getBytes() returns a bytearray
-        dos.flush();
-        dos.close();
-
-  System.out.println("baos.toString()-----: " + baos.toString());    	
-  System.out.println("--------------------------------------");
-
-        byte[] charset = baos.toByteArray(); //baos is a outputStream, der zum byteArray werden muss
-        String toBeReturnedString = Base64.getEncoder().encodeToString(charset);
-
-        toBeReturnedString = "0" + toBeReturnedString;//make BluePrint String again
-
-  System.out.println("toBeReturnedString-----: "+toBeReturnedString);          
-  System.out.println("--------------------------------------");
-
-          microsample = "0" + microsample; //add leading 0 for final comparision
-
-  System.out.println("is input = output -----: " + (toBeReturnedString == microsample));          
-  System.out.println("--------------------------------------");//this check fails don't know why
-
-        }
-	            catch(Exception e){
-            System.out.println("Invalid String Exception");
-        }
-    }
+}
 }
