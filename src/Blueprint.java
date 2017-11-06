0017d8ab42f7f4c1803becc060e2281a99b9d3ce
Calibretto <github.com@sentinelx.de>
2017-11-06 20:55:57 +0100

    object array zerstört

--- src/Blueprint.java
+++ src/Blueprint.java
@@ -4,19 +4,20 @@ public class Blueprint {
 //alle dinge im BP sind JSON-Objekte 
 	String labelofBP;
 	String item; // im BP steht ein "item", reason unknown
-	Entity[] stuffToBuild; //create class of Entitys
+	//error
+	Entity[] stuffToBuild = new Entity()[]; //create class of Entitys
+	//error end
 	IconsInBP icons; //array oder list - das ist hier die frage
 	String versionNumber; //könnte vllt. auch ein sehr sehr langer int werden. 
 	
 	Blueprint(){
-		//for (int i = 0; i<=3;i++) {icons = new IconsInBP[i];} //does not compile
 	}
 	
 	public void setLabelOfBP(String labelofBP_) {
 		this.labelofBP = labelofBP_;}
 	
-	public void setItemOfBP(String setItemOfBP_) {
-		this.item = setItemOfBP_;}
+	public void setItemOfBP() {//im BP steht in der obersten ebene immer eine itembezeichnung drin
+		this.item = "blueprint";}
 		
 	public void setVersionOfBP(String versionNumber_) {
 		this.versionNumber = versionNumber_;}
